9401964acf7a4e775f7547e3ea1a28409f3eeef9
Rostyslav Yatsura <floxaz1@gmail.com>
2019-06-22 11:09:07 +0200

    styled PageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from './Footer';
 
 const PageNotFound = () => (
-    <div>
-      <h1>404</h1>
-      This page doesn't exist
-      <Link to="/">Home</Link>
+  <div className="notFound">
+    <div className="notFound__wrapper">
+      <div className="notFound__content">
+        <img src="images/not-found.svg" alt="not-found" className="notFound__img" />
+        <p className="notFound__reason">Page not found</p>
+        <button className="notFound__btn">
+          <Link to="/" className="notFound__link">Home</Link>
+        </button>
+      </div>
     </div>
+    <Footer />
+  </div>
 );
 
 export default PageNotFound;
